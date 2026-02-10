8002d9aaff75fd9f233f04b36fae718e08872344
sergioTrabajo2025 <cuentasergiotrabajo@gmail.com>
2026-02-10 14:49:40 -0300

    Initial commit

---
+++ wpf1erprograma/wpf1erprograma/Views/MainWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Windows;
+using wpf1erprograma.Infrastructure;
+using wpf1erprograma.ViewModels;
+
+namespace wpf1erprograma.Views
+{
+    public partial class MainWindow : Window
+    {
+        private readonly MainViewModel _vm;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            var channel =
+              "cDAQ9189-1E05111Mod1/port0/line0:7";
+
+            var io = new NiDaqDigitalOutputService(channel, 8);
+
+            _vm = new MainViewModel(io);
+            DataContext = _vm;
+
+            Closed += (_, __) => _vm.Dispose();
+        }
+    }
+}
